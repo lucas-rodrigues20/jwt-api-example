54519e0d17c289f8263b8780bc61533f3b3f02f5
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-07-07 20:25:05 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const errorHandlers = require('./middlewares/errors');
@@ -9,7 +10,9 @@ const api = "/api/v1";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use(jwtAuth.authentication(api));
 
 app.get('/', (req, res) => {
